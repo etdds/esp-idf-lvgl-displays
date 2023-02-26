ccd0ee0b1b4ac2ae6af181abe1080bc7b578cd4f
Porthos <etdds@protonmail.com>
2023-02-26 19:08:09 +0000

    Minimal additions to get TDisplay building.

--- boards/display_factory.cpp
+++ boards/display_factory.cpp
@@ -1,11 +1,14 @@
 #include "display_factory.hpp"
 
 #include "t-display-s3.hpp"
+#include "ttgo-tdisplay.hpp"
 
 namespace LVGLDisplay {
   Display& DisplayFactory::active_display() {
 #ifdef CONFIG_LVGL_DISPLAY_TDISPLAY_S3
     static TDisplayS3 display;
+#elif CONFIG_LVGL_DISPLAY_TTGO_TDISPLAY
+    static TTGOTDisplay display;
 #else
   #error No active display defined.
 #endif
